be7cd16532596ff7c918ea82dcaabeb0ab434c0a
Connor Littleton <connorlittleton@wealthfront.com>
2024-01-04 00:39:28 -0800

    fix: set height of header equal to combobox in side-panel

--- components/new-dashboard/components/dashboard-1.tsx
+++ components/new-dashboard/components/dashboard-1.tsx
@@ -236,7 +236,7 @@ export function Dashboard({
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
           <ScrollArea className="h-screen">
-            <div className="flex items-center px-4 py-2">
+            <div className="flex h-[52px] items-center px-4 py-2">
               <h1 className="text-xl font-bold">Dashboard</h1>
             </div>
             <Separator />
