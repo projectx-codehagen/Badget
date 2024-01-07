3c32356878e6c4540c97880a658771ee2770f1a7
Amine S <amine.s3y@gmail.com>
2024-01-07 18:53:51 +0100

    feat: add homepage layout

--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -5,6 +5,10 @@ import { getCurrentUser } from "@/lib/session";
 import { NavBar } from "@/components/layout/navbar";
 import { SiteFooter } from "@/components/layout/site-footer";
 
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { Main } from './Main';
+
 type LayoutProps = {
   children: React.ReactNode;
 }
@@ -15,12 +19,10 @@ export default async function Layout({
   const user = await getCurrentUser();
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <Suspense fallback="...">
-        <NavBar user={user} items={marketingConfig.mainNav} scroll={true} />
-      </Suspense>
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
-    </div>
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
   );
 }
