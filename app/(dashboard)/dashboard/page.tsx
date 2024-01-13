f1e5f602e5259e7f70cf5d887732fd033caad433
Pablo Kitzberger <kitzberger.pablo@gmail.com>
2024-01-13 15:13:32 -0300

    ui: show dashboards on mobile

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -36,23 +36,7 @@ export default async function DashboardPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
+      <div className="flex flex-col">
         <Dashboard
           accounts={accounts}
           mails={mails}
