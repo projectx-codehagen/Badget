f047f3e0fae543b1dbe09c9223bcccc6d07399b5
sourabpramanik <shubpramanik241@gmail.com>
2024-01-24 21:22:43 +0530

    feat: added settings pages

---
+++ app/(dashboard)/dashboard/settings/account/page.tsx
@@ -0,0 +1,19 @@
+import { Separator } from "@/components/ui/separator";
+
+import { AccountForm } from "./account-form";
+
+export default function SettingsAccountPage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Account</h3>
+        <p className="text-sm text-muted-foreground">
+          Update your account settings. Set your preferred language and
+          timezone.
+        </p>
+      </div>
+      <Separator />
+      <AccountForm />
+    </div>
+  );
+}
---
+++ app/(dashboard)/dashboard/settings/layout.tsx
@@ -0,0 +1,63 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+import { Separator } from "@/components/ui/separator";
+import SettingSidebarNav from "@/components/layout/settings-sidebar-nav";
+
+export const metadata: Metadata = {
+  title: "Forms",
+  description: "Advanced form example using react-hook-form and Zod.",
+};
+
+const sidebarNavItems = [
+  {
+    title: "Profile",
+    href: "/dashboard/settings",
+  },
+  {
+    title: "Account",
+    href: "/dashboard/settings/account",
+  },
+  {
+    title: "Bank Account",
+    href: "/dashboard/settings/bank-account",
+  },
+  {
+    title: "Appearance",
+    href: "/dashboard/settings/appearance",
+  },
+  {
+    title: "Notifications",
+    href: "/dashboard/settings/notifications",
+  },
+  {
+    title: "Display",
+    href: "/dashboard/settings/display",
+  },
+];
+
+interface SettingsLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function SettingsLayout({ children }: SettingsLayoutProps) {
+  return (
+    <>
+      <div className="hidden space-y-6 p-10 pb-16 md:block">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+          <p className="text-muted-foreground">
+            Manage your account settings and set e-mail preferences.
+          </p>
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SettingSidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
