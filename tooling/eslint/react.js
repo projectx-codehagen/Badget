ab79864fb8cd04e945459e4343ecd468cebc6a9d
Matteo Badini <matteobad@users.noreply.github.com>
2024-01-26 06:15:44 +0100

    Feature/tooling (#90)
    * chore: add tooling to monorepo
    
    * chore: revert eslint + tsconfig to original conf
    
    * fix: manypkg
    
    * fix: manypkg
    
    * fix: pnpm dep lint + format
    
    * docs: update readme
    
    * ci: test ci pnpm
    
    * ci: trigger ci only on pr on main

---
+++ tooling/eslint/react.js
@@ -0,0 +1,25 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-key": "off",
+  },
+  globals: {
+    React: "writable",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+  },
+};
+
+module.exports = config;
