58d4cb34a8f30143cf7276e51d59522df07ffd2f
Alex Ghirelli <info@alexghirelli.it>
2024-01-27 15:34:33 +0100

    feat(db): Update database configuration and table names

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,19 +1,23 @@
 import * as dotenv from "dotenv";
 import type { Config } from "drizzle-kit";
 
-dotenv.config({
-  path: "../../.env.local",
-});
+dotenv.config({ path: "../../.env.local" });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
+const uri = [
+  "mysql://",
+  process.env.DB_USERNAME,
+  ":",
+  process.env.DB_PASSWORD,
+  "@",
+  process.env.DB_HOST,
+  ":3306/",
+  process.env.DB_NAME,
+  '?ssl={"rejectUnauthorized":true}',
+].join("");
 
 export default {
-  schema: "./schema",
+  schema: "./src/schema",
   driver: "mysql2",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
-  },
-  tablesFilter: ["badget_*"],
+  dbCredentials: { uri },
+  tablesFilter: ["projectx_*"],
 } satisfies Config;
--- packages/db/src/schema/_table.ts
+++ packages/db/src/schema/_table.ts
@@ -6,4 +6,4 @@ import { mysqlTableCreator } from "drizzle-orm/mysql-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const mySqlTable = mysqlTableCreator((name) => `badget_${name}`);
+export const mySqlTable = mysqlTableCreator((name) => `projectx_${name}`);
