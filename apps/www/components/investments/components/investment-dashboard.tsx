c0f11ad57e72e4cb9ded8eef19ae924d7d6fb02f
miralsuthar <miralsuthar2000@gmail.com>
2024-02-02 10:35:42 +0530

    fix: format code

--- apps/www/components/investments/components/investment-dashboard.tsx
+++ apps/www/components/investments/components/investment-dashboard.tsx
@@ -88,7 +88,7 @@ export function InvestmentsDashboard({
           }}
           className={cn(
             isCollapsed &&
-            "min-w-[50px] transition-all duration-300 ease-in-out",
+              "min-w-[50px] transition-all duration-300 ease-in-out",
           )}
         >
           <div
