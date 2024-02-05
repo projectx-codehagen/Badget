f7a6db6a4b4fc8c967adde832a4d9ef68746fd4a
meglerhagen <Christer.hagen@gmail.com>
2024-02-05 08:19:39 +0100

    lint error fix

--- apps/www/components/open-page/OpenCardsSection.tsx
+++ apps/www/components/open-page/OpenCardsSection.tsx
@@ -41,7 +41,9 @@ export default function OpenCardSection({ githubData }) {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Merged PR's</CardTitle>
+            <CardTitle className="text-sm font-medium">
+              Merged PR`&apos;`s
+            </CardTitle>
             <GitPullRequestIcon className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
