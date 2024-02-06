29470753fb62cff956611ab83fcd937e1e6cf67a
Matteo Badini <matteo.badini95@gmail.com>
2024-02-06 17:20:57 +0100

    feat(www): ui for adding integrations

--- packages/api/src/router/integrations.ts
+++ packages/api/src/router/integrations.ts
@@ -1,11 +1,12 @@
 import { eq, schema } from "@projectx/db";
 
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
-export const authRouter = createTRPCRouter({
-  listIntegrations: protectedProcedure.query(async (opts) => {
+export const integrationsRouter = createTRPCRouter({
+  listIntegrations: publicProcedure.query(async (opts) => {
     const integrations = await opts.ctx.db
       .select({
+        id: schema.integrations.id,
         name: schema.integrations.name,
         logoUrl: schema.integrations.logoUrl,
         connector: {
