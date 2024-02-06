358e7c69c3bea24c16420e7c3f88ead52416ec12
Alex Ghirelli <info@alexghirelli.it>
2024-02-06 23:31:53 +0100

    feat: add user schema to sync data from Clerk

--- packages/db/src/enum.ts
+++ packages/db/src/enum.ts
@@ -92,3 +92,12 @@ export const PrimaryCategory = {
 } as const;
 export type PrimaryCategory =
   (typeof PrimaryCategory)[keyof typeof PrimaryCategory];
+
+export const UserStatus = {
+  ACTIVE: "ACTIVE",
+  INACTIVE: "INACTIVE",
+  BLOCKED: "BLOCKED",
+  BANNED: "BANNED",
+  DELETED: "DELETED",
+} as const;
+export type UserStatus = (typeof UserStatus)[keyof typeof UserStatus];
