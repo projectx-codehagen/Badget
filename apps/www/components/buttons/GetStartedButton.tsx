ae36024b3b5a7354db518474468fe96ee7bc3152
Yaw Twumasi Acheampong <auxibee@gmail.com>
2024-02-07 19:28:30 -0500

    redirect authenticated user to dashboard

--- apps/www/components/buttons/GetStartedButton.tsx
+++ apps/www/components/buttons/GetStartedButton.tsx
@@ -4,13 +4,16 @@ import { cn } from "@/lib/utils";
 import { useSigninModal } from "@/hooks/use-signin-modal";
 import { Button, buttonVariants } from "@/components/ui/button";
 
+import {  useUser } from "@clerk/nextjs";
+import { redirect } from 'next/navigation'
+
 export function GetStartedButton() {
   const signInModal = useSigninModal();
-
+  const { isSignedIn } = useUser();
   return (
     <Button
       className={cn(buttonVariants({ size: "lg" }))}
-      onClick={signInModal.onOpen}
+      onClick={isSignedIn ? redirect('/dashboard') :signInModal.onOpen}
     >
       Get started
     </Button>
