cd03e667422314ee58799620baa2a670daa9de7f
Christer <Christer.hagen@gmail.com>
2024-02-14 05:56:02 +0100

    feat: update default layout for dashboard panels (#143)
    The default layout for the Transactions, Accounts, Dashboard, Recurring, and Categories dashboards has been updated. The previous default layout values [265, 440, 655] have been changed to [20, 40, 40]. This change was made to improve the initial layout and make it more visually appealing for users.

--- apps/www/app/(dashboard)/dashboard/investments/page.tsx
+++ apps/www/app/(dashboard)/dashboard/investments/page.tsx
@@ -18,23 +18,7 @@ export default async function DashboardPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
+      <div className="flex flex-col">
         <InvestmentsDashboard
           accounts={accounts}
           mails={mails}
