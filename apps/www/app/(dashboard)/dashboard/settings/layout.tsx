dccec0378f7d206c33eeb614ffc5534108b07e67
Piyush <75241694+piyusharmap@users.noreply.github.com>
2024-03-03 19:20:04 +0530

    Added a redirect link to dashboard on settings page. (#189)

--- apps/www/app/(dashboard)/dashboard/settings/layout.tsx
+++ apps/www/app/(dashboard)/dashboard/settings/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
 import SettingSidebarNav from "@/components/layout/settings-sidebar-nav";
@@ -47,7 +49,14 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="hidden space-y-6 px-10 py-4 pb-16 md:block">
+        <Link
+          href="/dashboard"
+          className="group -mx-2 flex w-fit items-center gap-1 opacity-75 hover:opacity-100"
+        >
+          <ChevronLeft className="ml-1 h-4 w-4 transition-all group-hover:ml-0 group-hover:mr-1" />{" "}
+          Dashboard
+        </Link>
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
