f5a86c5992b41248e3a1cf1f51a527780c36740e
meglerhagen <Christer.hagen@gmail.com>
2024-05-07 10:37:21 +0200

    renames and new components added

--- apps/www/app/(dashboard)/(workspaceId)/aimagic/actions.tsx
+++ apps/www/app/(dashboard)/(workspaceId)/aimagic/actions.tsx
@@ -16,9 +16,10 @@ import {
   BotMessage,
   SpinnerMessage,
 } from "./_components/chat-message";
-import { MonthlyBudgetTable } from "./_components/monthly-budget-table";
-import { MonthlySpendingChart } from "./_components/monthly-spending-chart";
-import { MonthlySpendingChartSkeleton } from "./_components/monthly-spending-chart-skeleton";
+import { HoldingsTableChat } from "./_components/genui/holdings-table";
+import { MonthlyBudgetTable } from "./_components/genui/monthly-budget-table";
+import { MonthlySpendingChart } from "./_components/genui/monthly-spending-chart";
+import { MonthlySpendingChartSkeleton } from "./_components/genui/monthly-spending-chart-skeleton";
 import type { AIState, UIState } from "./types";
 
 export const sleep = (ms: number) =>
@@ -58,15 +59,17 @@ const submitUserMessage = async (message: string) => {
       {
         role: "system",
         content: `\
-You are an AI-driven financial management bot, part of the Badget platform, designed to empower users with advanced insights and tools for stock trading and budget optimization. Your role is to help users make informed decisions about their investments and savings through interactive charts and a user-friendly interface.
+You are an AI-driven financial management bot, part of the Badget platform, designed to empower users with advanced insights and tools budget optimization. Your role is to help users make informed decisions about their investments and savings through interactive charts and a user-friendly interface.
 
 Messages inside [] are used to indicate UI elements or user interactions within the Badget platform. These visual cues help you navigate the interface and make informed decisions. For example:
 - "[Show Monthly Spending]" will display a detailed interactive chart of the user’s monthly spending.
-- "[Show Budget]" will display a detailed interactive chart of the user’s monthly budget.
+- "[Show Monthly Budget]" will display a detailed interactive chart of the user’s monthly budget.
+- "[Show My Holding]" will display a detailed interactive chart of the user’s holdings.
 - "[Adjust Budget to $X]" allows users to set a new budget amount, which is then reflected in their spending chart.
 
 To display the user's monthly spending chart, call \`showMonthlySpendingChart\`.
 To display the user's monthly budget, call \`showMonthlyBudget\`.
+To display the user's holdings, call \`showHoldings\`.
 
 Besides that, engage in discussions, provide financial insights, and perform calculations as users navigate their financial journeys.`,
       },
@@ -140,30 +143,9 @@ Besides that, engage in discussions, provide financial insights, and perform cal
         render: async function* (props) {
           console.log("props", props);
 
-          // Prepare a new message to be added to the conversation
-          // const newMessage = {
-          //   id: nanoid(),
-          //   role: "assistant",
-          //   content:
-          //     "Based on your previous spending, I guess that we are going to see some interesting trends in your monthly budget overview. Let's take a look...",
-          // };
-
           yield (
             <BotMessage content="Analyzing your spending patterns to create a monthly budget..." />
           );
-
-          // Update the state with the new message
-          // aiState.update({
-          //   ...aiState.get(),
-          //   messages: [...aiState.get().messages, newMessage],
-          // });
-
-          // Yield the updated state with all messages including the new one
-          // yield (
-          //   <BotCard>
-          //     <BotMessage content={newMessage.content} />
-          //   </BotCard>
-          // );
 
           // Give time for reading the message
           await sleep(1500);
@@ -196,6 +178,49 @@ Besides that, engage in discussions, provide financial insights, and perform cal
           return (
             <BotCard>
               <MonthlyBudgetTable />
+            </BotCard>
+          );
+        },
+      },
+      showHoldings: {
+        parameters: z.object({}),
+        description: "Show your holdings.",
+        render: async function* (props) {
+          console.log("props", props);
+
+          yield <BotMessage content="Looking at your portefolio..." />;
+
+          // Give time for reading the message
+          await sleep(1500);
+
+          // Proceed with showing the spinner
+          yield (
+            <BotCard>
+              <SpinnerMessage />
+            </BotCard>
+          );
+
+          // Simulate loading time
+          await sleep(1000);
+
+          // Update the state to signal the loading is done and include the new function message
+          aiState.done({
+            ...aiState.get(),
+            messages: [
+              ...aiState.get().messages,
+              {
+                id: nanoid(),
+                role: "function",
+                name: "showHoldings",
+                content: "Here is your holdings",
+              },
+            ],
+          });
+
+          // Show the final component
+          return (
+            <BotCard>
+              <HoldingsTableChat />
             </BotCard>
           );
         },
