065cc0b518fbb4bebd0cce21333ec522b81f8a6f
Shouryan Nikam <dev.shouryan@gmail.com>
2024-05-11 13:52:23 -0700

    move db connection string server side

--- apps/www/env.ts
+++ apps/www/env.ts
@@ -18,6 +18,7 @@ export const env = createEnv({
     // DATABASE_PASSWORD: z.string().min(1),
     STRIPE_API_KEY: z.string().min(1).optional(),
     STRIPE_WEBHOOK_SECRET: z.string().min(1).optional(),
+    DATABASE_URL: z.string().url(),
   },
   /**
    * Specify your client-side environment variables schema here.
@@ -29,7 +30,6 @@ export const env = createEnv({
     NEXT_PUBLIC_STRIPE_STD_MONTHLY_PRICE_ID: z.string().min(1).optional(),
     NEXT_PUBLIC_STRIPE_PRO_PRODUCT_ID: z.string().min(1).optional(),
     NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID: z.string().min(1).optional(),
-    NEXT_PUBLIC_DATABASE_URL: z.string().url(),
   },
   /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
@@ -46,7 +46,6 @@ export const env = createEnv({
       process.env.NEXT_PUBLIC_STRIPE_PRO_PRODUCT_ID,
     NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID:
       process.env.NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID,
-    NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
