953d23a180c41b9f6f17e7a8345ec40ca006b1e1
meglerhagen <Christer.hagen@gmail.com>
2024-07-24 21:38:21 +0200

    working tabs

--- apps/www/src/app/(dashboard)/dashboard/banking/page.tsx
+++ apps/www/src/app/(dashboard)/dashboard/banking/page.tsx
@@ -1,16 +1,22 @@
 import { redirect } from "next/navigation";
 import { getUserBankAccounts } from "@/actions/get-bankaccounts"; // Adjust the import path as needed
 
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@dingify/ui/components/tabs";
+
 import { authOptions } from "@/lib/auth";
 import { getCurrentUser } from "@/lib/session";
 import BankAccountsTable from "@/components/banking/BankAccountsTable";
+import { BankingDropdownMenu } from "@/components/banking/BankingDropdownMenu";
 import { AddAccountSheet } from "@/components/buttons/AddAccountSheeet";
-import { AddButton } from "@/components/buttons/AddButton";
 import { AreaChartBanking } from "@/components/charts/AreaChart";
 import { OverallUseageChart } from "@/components/charts/OverallUseageChart";
 import { DashboardHeader } from "@/components/dashboard/header";
 import { DashboardShell } from "@/components/dashboard/shell";
-import { CSVUploader } from "@/components/import/CsvImporter";
 import { EmptyPlaceholder } from "@/components/shared/empty-placeholder";
 
 export const metadata = {
@@ -30,32 +36,55 @@ export default async function BankingPage() {
 
   return (
     <DashboardShell>
-      <DashboardHeader heading="Banking" text="Overview off all your accounts ">
+      <DashboardHeader heading="Banking" text="Overview off all your accounts">
         <AddAccountSheet currentPath="/banking" />
       </DashboardHeader>
-      <div>
-        {bankAccounts.length === 0 ? (
-          <EmptyPlaceholder>
-            <EmptyPlaceholder.Icon name="post" />
-            <EmptyPlaceholder.Title>
-              You have no bank accounts
-            </EmptyPlaceholder.Title>
-            <EmptyPlaceholder.Description>
-              Add a bank account to start tracking your finances.
-            </EmptyPlaceholder.Description>
-            <AddAccountSheet currentPath="/banking" />
-          </EmptyPlaceholder>
-        ) : (
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <AreaChartBanking />
-            <OverallUseageChart />
-
-            <div className="col-span-1 md:col-span-2">
-              <BankAccountsTable bankAccounts={bankAccounts} />
-            </div>
+      <Tabs defaultValue="overview" className="-mt-4 w-full">
+        <div className="mb-4 flex items-center justify-between">
+          <TabsList>
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="accounts">Accounts</TabsTrigger>
+          </TabsList>
+          <BankingDropdownMenu />
+        </div>
+        <TabsContent value="overview">
+          <div>
+            {bankAccounts.length === 0 ? (
+              <EmptyPlaceholder>
+                <EmptyPlaceholder.Icon name="post" />
+                <EmptyPlaceholder.Title>
+                  You have no bank accounts
+                </EmptyPlaceholder.Title>
+                <EmptyPlaceholder.Description>
+                  Add a bank account to start tracking your finances.
+                </EmptyPlaceholder.Description>
+                <AddAccountSheet currentPath="/banking" />
+              </EmptyPlaceholder>
+            ) : (
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+                <AreaChartBanking />
+                <OverallUseageChart />
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </TabsContent>
+        <TabsContent value="accounts">
+          {bankAccounts.length === 0 ? (
+            <EmptyPlaceholder>
+              <EmptyPlaceholder.Icon name="post" />
+              <EmptyPlaceholder.Title>
+                You have no bank accounts
+              </EmptyPlaceholder.Title>
+              <EmptyPlaceholder.Description>
+                Add a bank account to start tracking your finances.
+              </EmptyPlaceholder.Description>
+              <AddAccountSheet currentPath="/banking" />
+            </EmptyPlaceholder>
+          ) : (
+            <BankAccountsTable bankAccounts={bankAccounts} />
+          )}
+        </TabsContent>
+      </Tabs>
     </DashboardShell>
   );
 }
